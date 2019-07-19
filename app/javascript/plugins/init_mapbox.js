6b07e79093d18a75ac847ed315ff1dffe8dec45f
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2019-07-19 07:13:03 +0100

    Fixed map issue for heroku

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,24 +11,24 @@ const buildMap = () => {
 };
 
 const addMarkersToMap = (map, markers) => {
-markers.forEach((marker) => {
+  markers.forEach((marker) => {
 
-  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
-  // Create a HTML element for your custom marker
-  const element = document.createElement('div');
-  element.className = 'marker';
-  element.style.backgroundImage = `url('${marker.image_url}')`;
-  element.style.backgroundSize = 'contain';
-  element.style.width = '25px';
-  element.style.height = '25px';
+    // Create a HTML element for your custom marker
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '25px';
+    element.style.height = '25px';
 
-  // Pass the element as an argument to the new marker
-  new mapboxgl.Marker(element)
-    .setLngLat([marker.lng, marker.lat])
-    .setPopup(popup)
-    .addTo(map);
-});
+    // Pass the element as an argument to the new marker
+    new mapboxgl.Marker(element)
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
+      .addTo(map);
+  });
 };
 
 const fitMapToMarkers = (map, markers) => {
