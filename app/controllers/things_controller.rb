49f365e36f07f7d91f2a321dc278de50b3fd6129
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2019-07-19 07:01:10 +0100

    Added unique markers to the map

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -7,8 +7,22 @@ class ThingsController < ApplicationController
       {
         lat: thing.latitude,
         lng: thing.longitude,
-        infoWindow: render_to_string(partial: "things/pop_up", locals: { thing: thing })
+        infoWindow: render_to_string(partial: "things/pop_up", locals: { thing: thing }),
+        image_url: helpers.asset_url(marker_image(thing.category))
       }
+    end
+  end
+
+  def marker_image(category)
+    case category
+      when "Coffeeshop"
+        "https://upload.wikimedia.org/wikipedia/commons/a/a8/Cannabis_leaf.svg"
+      when "Food"
+        "https://www.svgrepo.com/show/169724/burger.svg"
+      when "Bar"
+        "http://clipart-library.com/newimages/beer-mug-clip-art-9.png"
+      else
+        "https://image.flaticon.com/icons/svg/33/33622.svg"
     end
   end
 
@@ -23,7 +37,7 @@ class ThingsController < ApplicationController
   def create
     @thing = Thing.create(thing_params)
     if @thing.save
-      redirect_to thing_path(@thing)
+      redirect_to things_path
     else
       render :new
     end
